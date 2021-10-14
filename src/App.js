e39245e3164b4d3bc197ee09cf531858a2432fab
Kenny Bates <kennyleebates@outlook.com>
2021-10-14 12:52:09 -0400

    update App.js
    Update to grab static data for render of components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { React, useState} from 'react';
+import { data } from './data';
+import List from './List';
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <main>
+      <h1>Birthday Reminders</h1>
+      <section className="container">
+        <h2> {people.length} Birthdays today</h2>
+        <List people={people}/>
+      </section>
+      <button className="btn" >Clear Birthdays</button>
+    </main>
   );
 }
 
