ceea146613c17177200e5cd39423be09168a0bd1
Kenny Bates <kennyleebates@outlook.com>
2021-10-14 13:31:20 -0400

    update App.js
    added a button and a function to reset and append all birthdays to the screen.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import List from './List';
 
 function App() {
   const [people, setPeople] = useState(data);
+  const reset = () => {
+    setPeople(data);
+  };
   return (
     <main>
       <section className="container">
         <h3> {people.length} Birthdays today</h3>
         <List people={people}/>
         <button onClick={() => setPeople([])}>Clear All</button>
+        <button onClick={reset}>Show All</button>
       </section>
       
     </main>
