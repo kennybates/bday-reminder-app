be6e9130d6ee8883742ccd3f1cce871b3ac50383
Kenny Bates <kennyleebates@outlook.com>
2021-10-14 12:29:59 -0400

    Added data.js
    data.js contains an array for persons data.

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Betsy Sloan',
+        age: 41,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+
+    },
+    {
+        id: 2,
+        name: 'Nester Logan',
+        age: 34,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',  
+    },
+    {
+        id: 3,
+        name: 'Larry Neslon',
+        age: 37,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+        id: 4,
+        name: 'Sean Wallace',
+        age: 41,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+        id: 5,
+        name: 'Lila Grant',
+        age: 27,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+
+];
